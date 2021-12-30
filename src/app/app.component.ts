2ed03d28563d120a6db46188da6cc9d2bcdefac1
ln-dev7 <leonelngoya@gmail.com>
2021-12-30 13:46:41 +0100

    insertion des elements sous forme de tableau

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FaceSnap } from './models/face-snap.model';
 
@@ -8,26 +9,40 @@ import { FaceSnap } from './models/face-snap.model';
 })
 
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  otherSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
+  // mySnap!: FaceSnap;
+  // otherSnap!: FaceSnap;
   ngOnInit() {
-    this.mySnap = {
-      title: 'La joie',
-      description: 'Il faut toujours etre en joie !',
-      imageUrl: 'https://images.unsplash.com/photo-1545315003-c5ad6226c272?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218MHx8aGFwcHl8fHx8fHwxNjQwODM5OTg4&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300',
-      createDate: new Date(),
-      statusSnap: false,
-      snaps: 4,
-      location: 'Logbessou'
-    };
-    this.otherSnap = {
-      title: 'Les pattes',
-      description: 'Manger c\'est bien :)',
-      imageUrl: 'https://images.unsplash.com/photo-1536540166989-ad5334cee5f0?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218MHx8ZWF0fHx8fHx8MTY0MDg0NDQxOA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300',
-      createDate: new Date(),
-      statusSnap: false,
-      snaps: 13
-    };
+    this.faceSnaps = [
+      {
+        title: 'Personnal computer',
+        description: 'Ordinateur de travail personnel',
+        imageUrl: 'https://images.unsplash.com/photo-1586210579191-33b45e38fa2c?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218MHx8Y29tcHV0ZXJ8fHx8fHwxNjQwODY4MTA3&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300',
+        createDate: new Date(),
+        statusSnap: false,
+        snaps: 32,
+        location: 'My Zone'
+      },
+      {
+        title: 'La joie',
+        description: 'Il faut toujours etre en joie !',
+        imageUrl: 'https://images.unsplash.com/photo-1545315003-c5ad6226c272?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218MHx8aGFwcHl8fHx8fHwxNjQwODM5OTg4&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300',
+        createDate: new Date(),
+        statusSnap: false,
+        snaps: 4,
+        location: 'Logbessou'
+      },
+      {
+        title: 'Les pattes',
+        description: 'Manger c\'est bien :)',
+        imageUrl: 'https://images.unsplash.com/photo-1565895405140-6b9830a88c19?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218MHx8ZWF0fHx8fHx8MTY0MDg2ODMyMQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300',
+        createDate: new Date(),
+        statusSnap: false,
+        snaps: 13
+      }
+    ];
+    // this.mySnap = ;
+    // this.otherSnap = ;
   }
 }
 
