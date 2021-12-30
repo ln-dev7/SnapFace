79c331cabcfda1fcefba54fb487a5549e9854adf
ln-dev7 <leonelngoya@gmail.com>
2021-12-30 15:39:23 +0100

    ajout du style au bouton j'aime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,12 +15,12 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.faceSnaps = [
       {
-        title: 'Personnal computer',
+        title: 'PC ',
         description: 'Ordinateur de travail personnel',
         imageUrl: 'https://images.unsplash.com/photo-1586210579191-33b45e38fa2c?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218MHx8Y29tcHV0ZXJ8fHx8fHwxNjQwODY4MTA3&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300',
         createDate: new Date(),
         statusSnap: false,
-        snaps: 32,
+        snaps: 30,
         location: 'My Zone'
       },
       {
@@ -29,16 +29,16 @@ export class AppComponent implements OnInit {
         imageUrl: 'https://images.unsplash.com/photo-1545315003-c5ad6226c272?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218MHx8aGFwcHl8fHx8fHwxNjQwODM5OTg4&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300',
         createDate: new Date(),
         statusSnap: false,
-        snaps: 4,
+        snaps: 200,
         location: 'Logbessou'
       },
       {
-        title: 'Les pattes',
+        title: 'La tchop',
         description: 'Manger c\'est bien :)',
         imageUrl: 'https://images.unsplash.com/photo-1565895405140-6b9830a88c19?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218MHx8ZWF0fHx8fHx8MTY0MDg2ODMyMQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300',
         createDate: new Date(),
         statusSnap: false,
-        snaps: 13
+        snaps: 90
       }
     ];
     // this.mySnap = ;
